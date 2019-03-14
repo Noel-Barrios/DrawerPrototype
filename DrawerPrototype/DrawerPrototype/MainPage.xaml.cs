6b2066642d70313752456f710f39fe3aed2d2fa2
Noel Barrios <noel.barrios@optum.com>
2019-03-14 10:46:57 -0500

    add a hidden/slide-out view

--- DrawerPrototype/DrawerPrototype/MainPage.xaml.cs
+++ DrawerPrototype/DrawerPrototype/MainPage.xaml.cs
@@ -9,9 +9,46 @@ namespace DrawerPrototype
 {
     public partial class MainPage : ContentPage
     {
+        bool isSlideOutInView;
+
         public MainPage()
         {
+
             InitializeComponent();
         }
+
+        async void OnAddNewBtnClicked(object sender, EventArgs args)
+        {
+            if (isSlideOutInView == false)
+            {
+                addNewbtn.IsEnabled = false;
+                // animate the content to its original position (defined in corresponding xaml file)
+                addNewbtn.TranslateTo(-(slideOutScrollView.Width - addNewbtn.Width), 0, 1000);
+                await slideOutScrollView.TranslateTo(0, 0, 1000);
+                isSlideOutInView = true;
+                addNewbtn.IsEnabled = true;
+            }
+            else
+            {
+                addNewbtn.IsEnabled = false;
+                // animate the content out of view
+                addNewbtn.TranslateTo(0, 0, 1000);
+                await slideOutScrollView.TranslateTo(slideOutScrollView.Width, 0, 1000);
+                isSlideOutInView = false;
+                addNewbtn.IsEnabled = true;
+            }
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            //screenWidth = Application.Current.MainPage.Width;
+            // set the content out of the bounds of the screen
+            slideOutScrollView.TranslationX = (slideOutScrollView.Width);
+            isSlideOutInView = false;
+
+        }
+
     }
 }
